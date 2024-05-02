482bdac6d6f11336376ac6db9a88c56ce3e8d21e
Cristian Salvalaggio <99545448+crissalvalaggio@users.noreply.github.com>
2024-05-02 11:23:52 -0300

    valueerror

---
+++ python files/erro-exceção.py
@@ -0,0 +1,13 @@
+def exercicio():
+    while True:
+        try:
+            x = int(input("Digite um número: "))
+            break
+        except ValueError:
+            print("Entre com um numero válido.")
+exercicio()
+    
+
+    
+    
+
